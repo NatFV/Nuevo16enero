9a721200007b03a4117cf041f75090628a12ed07
NatFV <151751455+NatFV@users.noreply.github.com>
2024-01-16 11:14:16 +0100

    Update AccionTest.java
    Hacemos un pull

--- NuevoGit/src/main/java/Vista/AccionTest.java
+++ NuevoGit/src/main/java/Vista/AccionTest.java
@@ -6,6 +6,7 @@ public class AccionTest {
 		// TODO Auto-generated method stub
 		System.out.println ("Probando");
 		System.out.println ("Hacemos segundo commit");
+		System.out.println ("Hacemos un pull");
 	}
 
 }
